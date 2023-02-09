29067d39a60384fc49428db406ffceb104788a70
Jasmeet-Chicmic <jasmeet.sidhu@chicmic.co.in>
2023-02-09 19:06:40 +0530

    Node Pool implimented

--- assets/Scripts/characterController.ts
+++ assets/Scripts/characterController.ts
@@ -2,147 +2,148 @@ import {
   _decorator,
   Component,
   Input,
+  instantiate,
   lerp,
   Node,
+  NodePool,
+  Prefab,
+  random,
+  randomRangeInt,
   UITransform,
   Vec3,
 } from "cc";
+
 const { ccclass, property } = _decorator;
 let birdBounce: boolean = false;
 let hurdleStart: boolean = false;
+let birdAngle: boolean = false;
+
 @ccclass("characterController")
 export class characterController extends Component {
   @property({ type: Node })
   flappyBird: Node = null;
-
-  @property({ type: Node })
-  hurdleUp_1: Node = null;
-
-  @property({ type: Node })
-  hurdleDown_1: Node = null;
-
-  @property({ type: Node })
-  hurdleUp_2: Node = null;
-
-  @property({ type: Node })
-  hurdleDown_2: Node = null;
-
+  @property({ type: Prefab })
+  hurdle: Prefab = null;
   @property
+  hurdlePool = null;
+  @property
+  hurdle_NodePool = null;
+  @property
+  hurdleReRenderFlag = false;
+
+
   onLoad() {
     this.node.on(Input.EventType.TOUCH_START, this.birdController, this);
+    this.putNodesOnNodePool();
+
   }
-  start() {}
+
+  scheduleCreateHurdle() {
+    this.createHurdle();
+  }
+  putNodesOnNodePool() {
+    this.hurdlePool = new NodePool();
+    let init = 5;
+    for (let i = 0; i < init; i++) {
+      let hurdleInstance = instantiate(this.hurdle);
+      this.hurdlePool.put(hurdleInstance);
+
+    }
+    this.hurdleReRenderFlag = true;
+  }
+
+  createHurdle() {
+    if (this.hurdlePool.size() > 0) {
+      this.hurdle_NodePool = this.hurdlePool.get();
+      let pos = this.hurdle_NodePool.getPosition();
+      let canvasWidth = this.node.getComponent(UITransform).contentSize.width;
+
+      let hurdleWidth = this.hurdle_NodePool.getComponent(UITransform).width;
+      pos.x = (canvasWidth * 0.5 + hurdleWidth * 0.5)  //* randomRangeInt(1, 5);
+      pos.y = randomRangeInt(-5, 5) * 10;
+      this.hurdle_NodePool.setPosition(pos);
+      this.node.getChildByName("Hurdle").addChild(this.hurdle_NodePool);
+
+      // this.hurdlePool.put(this.hurdle_NodePool);
+    }
+
+    console.log("create hurdle chal riha");
+
+    hurdleStart = true;
+  }
+
+  start() { this.schedule(this.scheduleCreateHurdle, 0.8); }
+
   birdController() {
     hurdleStart = true;
     let birdOldPosition: Vec3 = this.flappyBird.getPosition();
-    // lerp(birdOldPosition.y, birdOldPosition.y + 100, 0.2);
+
     birdOldPosition.y = birdOldPosition.y + 50;
     let birdNewPostion: Vec3 = birdOldPosition;
     this.flappyBird.angle = 15;
-    // lerp(birdOldPosition.y, birdOldPosition.y + 50, 0.4);
     this.flappyBird.setPosition(birdNewPostion);
-    // console.log(birdNewPostion);
-
     birdBounce = true;
-    // setTimeout(this.setGoDownScheduler, 100);
-    // this.setGoDownScheduler();
+
+    setTimeout(() => {
+      birdAngle = true;
+    }, 100);
+
+
   }
 
-  //   setGoDownScheduler = () => {
-  //     this.flappyBird.angle = -30;
-  //     this.schedule(this.goDown, 0.1);
-  //   };
+
 
   goDown = (deltaTime) => {
-    this.flappyBird.angle = -30;
+    if (birdAngle == false) {
+      this.flappyBird.angle = -30;
+    }
     let birdOldPosition: Vec3 = this.flappyBird.getPosition();
     let canvasSize = this.node.getComponent(UITransform).contentSize.height;
-    let birdHeight =
-      this.node
-        .getComponent(characterController)
-        .flappyBird.getComponent(UITransform).contentSize.height /
-        2 +
-      3;
+    let birdHeight = (this.node.getComponent(characterController).flappyBird.getComponent(UITransform).contentSize.height) / 2 + 3;
 
-    if (
-      birdBounce == true &&
+    if (birdBounce == true &&
       Math.floor(birdOldPosition.y) >
-        -1 * Math.ceil(canvasSize / 2) + birdHeight
+      -1 * Math.ceil(canvasSize / 2) + birdHeight
     ) {
-      //   console.log((-1 * canvasSize) / 2);
-      console.log(Math.floor(birdOldPosition.y));
 
-      console.log(-1 * Math.ceil(canvasSize / 2) + birdHeight);
 
       lerp(birdOldPosition.y, birdOldPosition.y - 20, 0.01);
       birdOldPosition.y = birdOldPosition.y - 150 * deltaTime;
       let birdNewPostion: Vec3 = birdOldPosition;
-      // this.flappyBird.angle = 0;
       this.flappyBird.setPosition(birdNewPostion);
-      // console.log(birdNewPostion);
-      // console.log(Math.floor(birdOldPosition.y));
 
-      if (
-        Math.floor(birdOldPosition.y) ==
-        -1 * Math.ceil(canvasSize / 2) + birdHeight
-      ) {
-        // console.log("enter");
-
-        // this.unschedule(this.goDown);
+      if (Math.floor(birdOldPosition.y) == -1 * Math.ceil(canvasSize / 2) + birdHeight) {
         birdBounce = false;
       }
     }
   };
-  update(deltaTime: number) {
-    if (hurdleStart == true) {
-      let hurdleUp_1: Vec3 = this.hurdleUp_1.getPosition();
-      hurdleUp_1.x = hurdleUp_1.x - 100 * deltaTime;
-      this.hurdleUp_1.setPosition(hurdleUp_1);
 
-      let hurdleDown_1: Vec3 = this.hurdleDown_1.getPosition();
-      hurdleDown_1.x = hurdleDown_1.x - 100 * deltaTime;
-      this.hurdleDown_1.setPosition(hurdleDown_1);
+  hurdleMovement(deltaTime) {
+    this.flappyBird.angle = -10;
+    if (hurdleStart) {
+      let hurdleSize = this.node.getChildByName("Hurdle").children.length;
+      for (let i = 0; i < hurdleSize - 1; i++) {
+        let node = this.node.getChildByName("Hurdle").children[i];
+        let hurdleUp_1: Vec3 = node.getPosition();
+        hurdleUp_1.x = hurdleUp_1.x - 200 * deltaTime;
+        node.setPosition(hurdleUp_1);
+        let canvasWidth = this.node.getComponent(UITransform).contentSize.width;
 
-      if (hurdleUp_1.x < -500) {
-        hurdleUp_1.x = 500;
-        this.hurdleUp_1.setPosition(hurdleUp_1);
-        hurdleDown_1.x = 500;
-        this.hurdleDown_1.setPosition(hurdleDown_1);
-      }
-
-      let hurdleUp_2: Vec3 = this.hurdleUp_2.getPosition();
-      hurdleUp_2.x = hurdleUp_2.x - 100 * deltaTime;
-      this.hurdleUp_2.setPosition(hurdleUp_2);
-
-      let hurdleDown_2: Vec3 = this.hurdleDown_2.getPosition();
-      hurdleDown_2.x = hurdleDown_2.x - 100 * deltaTime;
-      this.hurdleDown_2.setPosition(hurdleDown_2);
-
-      if (hurdleUp_2.x < -500) {
-        hurdleUp_2.x = 500;
-        this.hurdleUp_2.setPosition(hurdleUp_2);
-        hurdleDown_2.x = 500;
-        this.hurdleDown_2.setPosition(hurdleDown_2);
+        let hurdleWidth = node.getComponent(UITransform).width;
+        if (node.getPosition().x < ((-1 * (canvasWidth) * 0.5)) - (hurdleWidth / 2)) {
+          this.hurdlePool.put(node);
+        }
       }
     }
-
-    this.goDown(deltaTime);
-    // let birdOldPosition:Vec3 = this.flappyBird.getPosition();
-    // let canvasSize = this.node.getComponent(UITransform).contentSize.height;
-    // let birdHeight = (this.node.getComponent(characterController).flappyBird.getComponent(UITransform).contentSize.height)/2;
-    // if(birdBounce == true && Math.floor(birdOldPosition.y) !== (-1 * Math.ceil(canvasSize/2)) + birdHeight){
-    //   console.log(-1 *canvasSize/2);
-    //    setTimeout(()=>{ birdOldPosition.y = birdOldPosition.y - 10;
-    //     let birdNewPostion:Vec3 = birdOldPosition;
-    //     this.flappyBird.angle = 0;
-    //     this.flappyBird.setPosition(birdNewPostion)
-    //     console.log(birdNewPostion);
-    //     console.log(Math.floor(birdOldPosition.y));
-    //    },100);
-    //    if( Math.floor(birdOldPosition.y) == (-1 * Math.ceil(canvasSize/2)) + birdHeight)
-    //    {
-    //     birdBounce = false;
-    //    }
-    // }
   }
+
+  update(deltaTime: number) {
+    // if (this.hurdleReRenderFlag) {
+    //   this.createHurdle();
+    // }
+
+    this.hurdleMovement(deltaTime);
+    this.goDown(deltaTime);
+  }
+
 }
